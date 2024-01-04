cce64fc3e57bd651b5fe4554c042c30980eb98f3
Ertuğ Akman <134059022+ertugakmann@users.noreply.github.com>
2024-01-04 22:07:28 +0300

    Update SavedCounts.jsx
    I changed some HTML codes and added MUI style codes.

--- ScoreBoard-React/src/components/SavedCounts.jsx
+++ ScoreBoard-React/src/components/SavedCounts.jsx
@@ -1,17 +1,18 @@
+import { Box, Typography } from "@mui/material";
 import Board from "./Board";
 
 function SavedCounts({ boards }) {
   return (
-    <div>
-      <div className="saved-counts">
-        <h1>SAVED COUNTS</h1>
-      </div>
-      <div className="saved-box">
+    <Box>
+      <Typography variant="h4" sx={{ mt: "50px" }}>
+        SAVED COUNTS
+      </Typography>
+      <Box sx={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
         {boards.map((board) => {
           return <Board board={board} key={board.id} />;
         })}
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 }
 
